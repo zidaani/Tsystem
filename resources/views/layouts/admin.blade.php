665421392e90d40c7b334867ea059279c8903b34
dokunusmoothie <dokunusmoothie@gmail.com>
2019-07-19 14:59:34 +0000

    admin.blade.php editted

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -754,4 +754,6 @@
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js') }}"></
+<script src="{{ asset('dist/js/demo.js') }}"></script>
+</body>
+</html>
